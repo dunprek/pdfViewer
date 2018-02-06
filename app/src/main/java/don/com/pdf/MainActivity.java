6bd1db4c8a9ba7a5bb8d91c78c14b98a34cbb177
gideon <dunprek@gmail.com>
2018-02-06 18:02:33 +0700

    add permission

--- app/src/main/java/don/com/pdf/MainActivity.java
+++ app/src/main/java/don/com/pdf/MainActivity.java
@@ -1,13 +1,21 @@
 package don.com.pdf;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import es.voghdev.pdfviewpager.library.PDFViewPager;
 import es.voghdev.pdfviewpager.library.adapter.PDFPagerAdapter;
@@ -20,13 +28,19 @@ public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
 
+    public static final int MULTIPLE_PERMISSIONS = 10; // code you want.
+
+
+    String[] permissions = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CopyAsset copyAsset = new CopyAssetThreadImpl(MainActivity.this, new Handler());
-        copyAsset.copy(getPDFPathonSDCard(), new File(getCacheDir(), "Proposal.pdf").getAbsolutePath());
-
+        checkPermission();
     }
 
     @Override
@@ -43,9 +57,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private boolean checkPermission() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(getApplicationContext(), p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CopyAsset copyAsset = new CopyAssetThreadImpl(MainActivity.this, new Handler());
+                    copyAsset.copy(getPDFPathonSDCard(), new File(getCacheDir(), "Proposal.pdf").getAbsolutePath());
 
-
+                } else {
+                    finish();
+                }
+                return;
+            }
+        }
+    }
 
 
 }
